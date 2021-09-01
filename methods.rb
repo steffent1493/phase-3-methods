b305afdab3c3ac935bcebf6a73a1fdf3ad64d1fe
steffent1493 <steffent1493@gmail.com>
2021-09-01 14:04:54 -0400

    mission accomplished

--- methods.rb
+++ methods.rb
@@ -1,1 +1,36 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+
+# function greetWithDefault(name = "programmer") {
+#   console.log(`Hello, ${name}!`);
+# }
+
+def greet_with_default (name = "programmer")
+    puts "Hello, #{name}!"
+end
+
+# function add(num1, num2) {
+#   return num1 + num2;
+# }
+
+def add(num1, num2)
+    return num1 + num2
+end
+
+# function halve(number) {
+#   if (typeof number !== "number") return null;
+
+#   return number / 2;
+# }
+
+def halve(num)
+    return nil unless num.class == Integer
+    
+    num / 2
+  end
